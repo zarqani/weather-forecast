6ea2273a904915203e9968630a78cdf322c02d5c
Fahime Zarqani <54211016+zarqani@users.noreply.github.com>
2023-12-25 10:12:21 +0330

    Update index.tsx

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+
+import { useEffect, useState } from "react";
 import Forecast from "./Forecast";
 import WeatherCard from "./WeatherCard";
 import { WeatherProps } from "@/types/weather.types";
 
 export default function Weather({ weather }: { weather: WeatherProps }) {
+  const [location,setLocation] = useState(null);
+
+    useEffect(()=>{
+    if (lnavigator && lnavigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        setLocation({ lat, lon });
+      },
+      (error) => {
+        console.error("Error getting location:", error.message);
+      }
+    );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+  
   return (
     <>
-      <WeatherCard data={weather} />
-      <Forecast />
+      <WeatherCard data={weather} location={location} />
+      <Forecast location={location} />
     </>
   );
 }
